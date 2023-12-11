8ddf44ff98c63cb27f8a1304ead5ae896e747423
hyperv <hyperv@ZG-D5-19>
2023-12-11 14:07:27 +0100

    Add project files.

---
+++ Vjezba_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Vjezba_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite recenicu:");
+            var tekst = Console.ReadLine();
+            Console.WriteLine();
+
+            tekst.Split(new char[] { ' ', ';', '.', ',', '?','!' }).GroupBy(x => x.ToLower()).ToList().ForEach(g => 
+            {
+                Console.WriteLine($"\"{g.Key}\" - se pojavljuje {g.Count()} puta");
+            });
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
